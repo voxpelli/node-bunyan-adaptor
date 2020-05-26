0c2f21c59e360a2a3dff0529bc39bda789ee8e3d
Pelle Wessman <pelle@kodfabrik.se>
2020-05-26 10:20:34 +0200

    Add superset type test for Pino and Bunyan
    Bunyan is failing sadly

---
+++ test/types.test-d.ts
@@ -0,0 +1,18 @@
+// This file will never run, only just be type validated and serve as a test suite for types
+
+import 'node';
+
+import { BunyanLite } from '..';
+
+// Will cause errors if used with something that's not a strict superset of BunyanLite
+type ExpectBunyanLite<T extends BunyanLite> = T;
+
+// Passes
+import { Logger as PinoLogger } from 'pino';
+export type PinoTest = ExpectBunyanLite<PinoLogger>;
+
+// Fails, but only because maxNodeModuleJsDepth is set in tsconfig.json
+// import bunyan = require('@scoop/bunyan');
+// const bunyanLogger = bunyan.createLogger();
+// export type BunyanTest = ExpectBunyanLite<typeof bunyanLogger>;
+
